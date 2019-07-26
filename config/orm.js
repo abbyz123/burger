fddff72b89a89bfd47269dfebe06a677059ad98a
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-07-25 19:03:56 -0700

    add log in all arm for troubleshooting

--- config/orm.js
+++ config/orm.js
@@ -43,6 +43,7 @@ function objToSql(ob) {
 var orm = {
   all: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
+    console.log(queryString);
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
